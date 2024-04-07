3eec823663f0c08849b571ec24575fc85bdef01b
WizMager <wizmager@gmail.com>
2024-04-07 17:11:07 +0300

    -complete with attack system

--- Assets/Scripts/Common/NpcAttackSystem.cs
+++ Assets/Scripts/Common/NpcAttackSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.NetCode;
 using Unity.Transforms;
 
@@ -9,7 +10,24 @@ namespace Common
     [UpdateInGroup(typeof(PredictedSimulationSystemGroup))]
     public partial struct NpcAttackSystem : ISystem
     {
-        
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<NetworkTime>();
+            state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+            var networkTime = SystemAPI.GetSingleton<NetworkTime>();
+
+            state.Dependency = new NpcAttackJob
+            {
+                CurrentTick = networkTime.ServerTick,
+                TransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(),
+                ECB = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter()
+            }.ScheduleParallel(state.Dependency);
+        }
     }
     
     [BurstCompile]
@@ -34,8 +52,23 @@ namespace Common
             var canAttack = !cooldownExpirationTick.Value.IsValid || CurrentTick.IsNewerThan(cooldownExpirationTick.Value);
             
             if (!canAttack) return;
+
+            var spawnPosition = TransformLookup[npcEntity].Position + attackProperty.FirePointOffset;
+            var targetPosition = TransformLookup[targetEntity.Value].Position;
+
+            var newAttack = ECB.Instantiate(sortKey, attackProperty.AttackPrefab);
+            var newAttackTransform = LocalTransform.FromPositionRotation(spawnPosition,
+                quaternion.LookRotationSafe(targetPosition - spawnPosition, math.up()));
             
-            
+            ECB.SetComponent(sortKey, newAttack, newAttackTransform);
+            ECB.SetComponent(sortKey, newAttack, team);
+
+            var newCooldownTick = CurrentTick;
+            newCooldownTick.Add(attackProperty.CooldownTickCount);
+            npcAttackCooldown.AddCommandData(new NpcAttackCooldown
+            {
+                Tick = CurrentTick, Value = newCooldownTick
+            });
         }
     }
 }
